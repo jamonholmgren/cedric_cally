d0b50451be73f5d327816eeb3bff14b4055469f6
Jamon Holmgren <jamon@clearsightstudio.com>
2014-06-06 20:24:32 -0700

    Init

---
+++ cedric.rb
@@ -0,0 +1,12 @@
+# Cedric and Cally's first program
+# 6/6/2014 at 7:30 PM
+# With Dad's help
+
+puts "cedric is cool"
+puts "cally loves you"
+name = gets
+puts "hi, " + name
+5.times do
+  puts "-" * 15
+  sleep 1
+end
---
+++ madlib.rb
@@ -0,0 +1,24 @@
+# Madlib by Cedric and Cally
+# with Dad's help 6/6/2014
+
+puts "Type a person or thing:"
+thing = gets
+puts " Type a thing in a city:"
+street = gets
+puts "animal:"
+animal = gets
+puts "action verb ending in 'ing' like 'running':"
+verbing = gets
+puts "Type of airplane:"
+airplane = gets
+puts "Action that ends in 'ed' like 'jumped':"
+actioned = gets
+puts "Another animal:"
+other_animal = gets
+puts "Verb, like 'ate':"
+verb = gets
+
+puts "One day, a " + thing + " was walking down the " + street + ""
+puts "when it saw a " + animal + " " + verbing + " alongside the " + airplane +"."
+puts "It immediately " + actioned + " and then the " + other_animal + " came and"
+puts "" + verb + " it."
